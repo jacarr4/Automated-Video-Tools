7a3fc27aa17ed861e9474bb0738ff3c6d0dd959b
Jake Carr <jakecarr@Jakes-Mac-mini.local>
2021-03-11 19:52:05 -0500

    url downloads working

--- scratch.py
+++ scratch.py
@@ -1,45 +1,25 @@
-from bs4 import BeautifulSoup
-import requests
+import json
 
-html_doc = """<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+JSON_FILE = 'clips_file.json' # input
+URL_FILE = 'url_file.txt' # output
+NUM_CLIPS = 10
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+def get_urls( json_file ):
+    urls = []
 
-<p class="story">...</p>
-"""
+    with open( json_file, 'r' ) as f:
+        json_data = json.loads( f.read() )
+        for clip_object in json_data[ 0 ][ 'data' ][ 'game' ][ 'clips' ][ 'edges' ]:
+            urls.append( clip_object[ 'node' ][ 'url' ] )
 
-URL = "https://www.twitch.tv/directory/game/Just%20Chatting/clips?range=all"
+    return urls
 
-CURL_URL = """curl 'https://gql.twitch.tv/gql' \
-  -H 'Connection: keep-alive' \
-  -H 'Authorization: OAuth v3sxp8w65fs7e111oofu4hvyyuwukt' \
-  -H 'Accept-Language: en-US' \
-  -H 'Client-Id: kimne78kx3ncx6brgo4mv6wki5h1ko' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36' \
-  -H 'X-Device-Id: CbHLeGQK5dSUl8DO5f02Fd6drdDwpymj' \
-  -H 'Content-Type: text/plain;charset=UTF-8' \
-  -H 'Accept: */*' \
-  -H 'Sec-GPC: 1' \
-  -H 'Origin: https://www.twitch.tv' \
-  -H 'Sec-Fetch-Site: same-site' \
-  -H 'Sec-Fetch-Mode: cors' \
-  -H 'Sec-Fetch-Dest: empty' \
-  -H 'Referer: https://www.twitch.tv/' \
-  --data-raw '[{"operationName":"ClipsCards__Game","variables":{"gameName":"Just Chatting","limit":20,"criteria":{"languages":[],"filter":"ALL_TIME"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"0d8d0eba9fc7ef77de54a7d933998e21ad7a1274c867ec565ac14ffdce77b1f9"}}}]' \
-  --compressed"""
+def write_urls( urls, url_file ):
+    with open( url_file, 'w' ) as f:
+      for url in urls:
+          f.write( f'{url}\n' )
 
 if __name__ == '__main__':
-    # soup = BeautifulSoup(html_doc, 'html.parser')
-    # print( soup.prettify() )
-
-    # print( URL )
-    page = requests.get( CURL_URL )
-    soup = BeautifulSoup( page.content, 'html.parser' )
-    print( soup.prettify() )
-    print( soup.find_all( 'a' ) )
+    urls = get_urls( JSON_FILE )
+    urls = urls[ :NUM_CLIPS ]
+    write_urls( urls, URL_FILE )
--- scratch.sh
+++ scratch.sh
@@ -1,14 +1,10 @@
-# TEXT="$TEXT$STR"
-# FIELD="TEXT=$TEXT&Action=post"
-# curl 'http://bord.ece498kl-fa2018.org/jacarr4/v1/index.php' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://bord.ece498kl-fa2018.org/jacarr4/v1/index.php' -H 'Content-Type: application/x-www-form-urlencoded' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Cookie: PHPSESSID=0unq04ivqare2jvbpun2i1sihj' -H 'Upgrade-Insecure-Requests: 1' --data $FIELD
-
-# TEXT="https://gql.twitch.tv"
-# FIELD='\[\{"operationName":"ClipsCards__Game","variables":\{"gameName":"Just Chatting","limit":20,"criteria":\{"languages":\[\],"filter":"ALL_TIME"\}\},"extensions":\{"persistedQuery":\{"version":1,"sha256Hash":"0d8d0eba9fc7ef77de54a7d933998e21ad7a1274c867ec565ac14ffdce77b1f9"\}\}\}\]'
-# FIELD="TEXT=$TEXT&$FIELD&Action=post"
-# curl 'https://gql.twitch.tv' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0' -H 'Accept: */*' -H 'Accept-Language: en-US' -H 'Referer: https://www.twitch.tv/' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Connection: keep-alive' --data $FIELD
-
-
-
+# STEPS TO REPRODUCE: 
+# load the twitch page with the clips you are looking for
+# click 'inspect element'
+# go to the network tab
+# reload the page
+# look through the requests until you find the one with the URLs
+# right click -> copy -> copy as cURL
 curl 'https://gql.twitch.tv/gql' \
   -H 'Connection: keep-alive' \
   -H 'Authorization: OAuth v3sxp8w65fs7e111oofu4hvyyuwukt' \
@@ -25,4 +21,4 @@ curl 'https://gql.twitch.tv/gql' \
   -H 'Sec-Fetch-Dest: empty' \
   -H 'Referer: https://www.twitch.tv/' \
   --data-raw '[{"operationName":"ClipsCards__Game","variables":{"gameName":"Just Chatting","limit":20,"criteria":{"languages":[],"filter":"ALL_TIME"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"0d8d0eba9fc7ef77de54a7d933998e21ad7a1274c867ec565ac14ffdce77b1f9"}}}]' \
-  --compressed > clips_file.html
+  --compressed > clips_file.json
